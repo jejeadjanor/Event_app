9544bd43b4fd76ee3d60edc1c5dd4bccdf0110a7
Glory GUEZO-MEVO <60992924+GloryX98@users.noreply.github.com>
2020-09-10 03:42:45 +0000

    First commit

---
+++ backend/signupapi/signupapi/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/',include('signup.urls')),
+    path('login/',include('login.urls')),
+
+    path('.auth/', obtain_auth_token)
+
+]
---
+++ myapp/src/components/LoginForm.js
@@ -0,0 +1,127 @@
+import React        from 'react';
+import InputField   from './InputField';
+import SubmitButton from './SubmitButton';
+import UserStore    from '../stores/UserStore';
+import { Link } from 'react-router-dom';
+
+class LoginForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            buttonDisabled: false
+        }
+    } 
+// PROPERTY HERE REFERS TO THE 'EMAIL' AND 'PASSWORD'
+    setInputValue(property, val) {
+        val = val.trim();
+        if(val.length < 0){
+            return;
+        }
+        this.setState({
+            [property]: val
+        })
+    }
+    resetForm() {
+        this.setState({
+            email: '',
+            password: '',
+            buttonDisabled: false
+        })
+    }
+    // API CALL FOR THE LOGIN BUTTON
+    async doLogin() {
+        if (!this.state.email){
+            return;
+        }
+        if (!this.state.password){
+            return;
+        }
+
+        this.setState({
+            buttonDisabled: true
+        })
+
+        try {
+           
+            let res = await fetch('http://127.0.0.1:8000/login/users/', {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+
+                body: JSON.stringify({
+                    email: this.state.email,
+                    password: this.state.password
+                })
+            });
+
+            let result = await res.json();
+            if (result && result.success) {
+                UserStore.isLoggedIn = true;
+                UserStore.email = result.email;
+            }
+
+            else if (result && result.success === false) {
+                this.resetForm();
+                alert (result.msg);
+            }
+        }
+
+        catch(e) {
+            console.log(e);
+            this.resetForm();
+
+        }
+    }
+    render(){  
+    // USER INTERFACE FOR THE LOGIN FORM
+        return(
+            
+            <div className="LoginForm">
+                <div className='log'> Sign into your account </div >
+
+                {/* THE INPUT FIELD FOR EMAIL ADDRESS */}
+                <div className="input-group">
+            <span><i className="fa fa-user" aria-hidden="true"></i></span>
+                    <InputField
+                        type = 'email'
+                        placeholder = 'Email *'
+                        required = {true}
+                        value = { this.state.email ? this.state.email: ''}
+                        onChange = { (val) => this.setInputValue('email', val)}
+                />
+                </div>
+                {/* THE INPUT FIELD FOR PASSWORD */}
+                <div className="input-group">
+            <span><i className="fa fa-lock" aria-hidden="true"></i></span>
+                    <InputField
+                        type = 'Password'
+                        placeholder = 'Password *' 
+                        required = {true}
+                        value = { this.state.password ? this.state.password: ''}
+                        onChange = { (val) => this.setInputValue('password', val)}
+                    />
+                    </div>
+                {/* THE LOG IN BUTTON */}
+                    <SubmitButton
+                        text = 'Sign in to your account'
+                        disabled = { this.state.buttonDisabled}
+                        onClick = { () => this.doLogin()}
+                        />
+                {/* THE CODE BELLOW CREATES A HORIZONTAL LINE */}
+                    <hr className='line'></hr>
+                        
+                        <div className= 'sign'>
+                            Don't have an account? 
+                            <Link to = {'/'}> Sign Up!</Link>
+                        </div>
+            </div>
+            
+        );
+    }
+}
+
+export default LoginForm;
